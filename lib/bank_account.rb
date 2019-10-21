0c32750c822bef921976aa045268b716a73e8f80
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-21 15:28:53 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
 class BankAccount
+attr_reader :name
+attr_accessor :balance, :status
+
+def initialize(name)
+  @name = name
+  @balance = 1000
+  @status = "open"
+end
+
+def deposit(amount)
+  @balance += amount
+end
+
+def display_balance
+  puts "Your balance is $#{@balance}."
+end
 
 end
