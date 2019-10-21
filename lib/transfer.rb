eb32a480705e542ac273324d17ad09066e408f03
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-21 19:21:27 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,23 @@ attr_accessor :sender, :receiver, :amount, :status
   end
 
   def execute_transaction
-
+    if !valid? || @sender.balance < @amount
+       @status = "rejected"
+       return "Transaction rejected. Please check your account balance."
+    elsif @status == "complete"
+      puts "Transaction was already executed"
+    else
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+   end
   end
+def reverse_transfer
+  if @status == "complete"
+    @sender.deposit(@amount)
+    @receiver.deposit(@amount * -1)
+    @status = "reversed"
+  end
+end
 
 end
